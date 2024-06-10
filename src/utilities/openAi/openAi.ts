f1fe5d792a0f806c437b7b27ddc6ae9a259e4905
Khalid Hafeez <khalidabdulhafeez@punh.agency>
2024-06-10 18:25:28 +0100

    removed  secret

--- src/utilities/openAi/openAi.ts
+++ src/utilities/openAi/openAi.ts
@@ -1,6 +1,5 @@
 import OpenAI from 'openai';
 
 export const openAIService: any = new OpenAI({
-  apiKey: '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',
-  //   apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
