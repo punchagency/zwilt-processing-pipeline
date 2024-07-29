1f840e7c5dcbe789c18b31fe71ed6744d74f60c5
Khalid Hafeez <khalidabdulhafeez@punh.agency>
2024-07-29 18:31:57 +0100

    ...

--- src/utilities/cron/backgroundTask.ts
+++ src/utilities/cron/backgroundTask.ts
@@ -37,7 +37,7 @@ export class BackgroundTask extends Tasks {
         this.processQuestionSummary();
       });
 
-      cron.schedule("*/17 * * * *", () => {
+      cron.schedule("*/15 * * * *", () => {
         // execute every 20 minutes
         this.downloadAndProcessVideoTranscribe();
       });
